f93c4a46728dfa230d429108490eb9ab67eb70d3
e <1750951064@qq.com>
2019-09-21 17:46:55 +0800

    最终版

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -1,6 +1,13 @@
-var BASE_URL = 'http://localhost:8000';
-var URLIST = {
-  user_login: BASE_URL + '/admin/login',
-  user_logout: BASE_URL + '/admin/logout',
-  user_info: BASE_URL + '/admin/getuser'
-};
+// var BASE_URL = 'http://localhost:8000';
+// var URLIST = {
+//   user_login: BASE_URL + '/admin/login',
+//   user_logout: BASE_URL + '/admin/logout',
+//   user_info: BASE_URL + '/admin/getuser'
+// };
+
+var base_url = 'http://localhost:8000';
+var urlist = {
+  user_login : base_url + '/admin/login',
+  user_logout: base_url + '/admin/logout',
+  user_info: base_url + '/admin/getuser'
+}
--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -1,27 +1,49 @@
+// var user = {
+//   /**
+//    * 管理员登陆
+//    * @param {*} userName 用户名
+//    * @param {*} password 密码
+//    * @param {*} callBack 回调函数
+//    */
+//   login: function(userName, password, callBack) {
+//     // alert(userName + password);
+//     $.post(URLIST.user_login, { user_name: userName, password: password }, function(res) {
+//       // res 是本次请求，从服务器回来的数据
+//       callBack(res);
+//     });
+//   },
+
+//   logout: function(callBack) {
+//     $.post(URLIST.user_logout, function(res) {
+//       callBack(res);
+//     });
+//   },
+
+//   getInfo: function(callBack) {
+//     $.get(URLIST.user_info, function(res) {
+//       callBack(res);
+//     });
+//   }
+// };
+
 var user = {
-  /**
-   * 管理员登陆
-   * @param {*} userName 用户名
-   * @param {*} password 密码
-   * @param {*} callBack 回调函数
-   */
-  login: function(userName, password, callBack) {
-    // alert(userName + password);
-    $.post(URLIST.user_login, { user_name: userName, password: password }, function(res) {
-      // res 是本次请求，从服务器回来的数据
+  // 管理员登录
+  login:function(userName,password,callBack){
+    $.post(urlist.user_login ,{"user_name":userName,"password":password},function(res){
       callBack(res);
-    });
+    })
   },
 
-  logout: function(callBack) {
-    $.post(URLIST.user_logout, function(res) {
-      callBack(res);
-    });
+  logout:function(callBack){
+    $.post(urlist.user_logout,function(res){
+     callBack(res);
+    })
   },
 
-  getInfo: function(callBack) {
-    $.get(URLIST.user_info, function(res) {
+  getInfo:function(callBack){
+    $.get(urlist.user_info,function(res){
       callBack(res);
-    });
+    })
   }
-};
+
+}
